733df0c313425ec225c68f65a61517fcbe953d8d
Bruno Plamondon <bplamondon66@gmail.com>
2023-09-14 11:56:02 -0400

    Remove Orientation component

--- lib/ants/components/orientation.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Ants
-  module Components
-    class Orientation < Draco::Component
-      attribute :degrees, default: 0 # in degrees (0..360)
-    end
-  end
-end
--- lib/ants/entities/ant.rb
+++ lib/ants/entities/ant.rb
@@ -7,7 +7,6 @@ module Ants
     class Ant < Draco::Entity
       component Components::Ai, behaviors: [Ants::Behaviors::Trees::Ant::Wander.tree]
       component Components::Motion
-      component Components::Orientation
       component Components::Position
       component Components::Renderable, renderer: Ants::Renderers::Ant.new
     end
